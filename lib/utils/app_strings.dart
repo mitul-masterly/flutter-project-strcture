cba84771a3b3c342f7066f4086bfadbb2fcad7b4
Bhoomi Patel <bhoomi.p@masterlysolutions.com>
2025-03-31 11:24:20 +0530

    Added android setup in git workflow

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -7,6 +7,8 @@ class AppStrings {
   static const String confirmPassword = 'Confirm Password';
   static const String firstName = 'First Name';
   static const String lastName = 'Last Name';
+  static const String enterTxt = 'Enter your';
+  static const String submit = 'Submit';
   static const String mobileNumber = 'Mobile Number';
   static const String globalErrorMessage = 'Something Went Wrong..!';
 }
